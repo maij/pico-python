44a56d8f32fc0fa3ac3c273bb931b4cfe7cf5c8b
morgatrons@gma <morgan.hedgeil.com>
2015-12-12 14:53:43 -0500

    Tweaked the example to use higher resolution (so that at the very least some noise should be observable!)

--- examples/test_ps5000.py
+++ examples/test_ps5000.py
@@ -6,10 +6,10 @@ import pylab as pl
 #Open the scope
 ps=ps5000a.PS5000a()
 
-# Capture 10 traces from channel A at sample rate of 5MS/s , each 2 ms long
+# Capture 10 traces from channel A at sample rate of 1MS/s , each 100 ms long
 # The scope triggers by default at 1V off a rising edge on the External connector.
 ps.quickSetup(chanAParams=dict(coupling="DC", VRange=5.0), nCaps=10, 
-        sampleRate=5e6, acqTime=2e-3,
+        sampleRate=1e6, acqTime=100e-3,resolution=15,
         triggerParams=dict(trigSrc="External", threshold_V=1.0, 
                 direction="Rising", delay=0)
           )
